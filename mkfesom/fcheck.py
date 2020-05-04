9b2b64303285b3f48a9a1a9bad41320eea32e4e1
Nikolay Koldunov <koldunovn@gmail.com>
2020-05-04 16:42:21 +0000

    finish fcheck

--- mkfesom/fcheck.py
+++ mkfesom/fcheck.py
@@ -4,7 +4,7 @@ import argparse
 import glob
 from netCDF4 import Dataset
 from pytest import approx
-
+import pandas as pd
 
 def find_last_year(path, var, runid='fesom'):
     files = glob.glob(os.path.join(path, f'{var}.{runid}.*.nc'))
@@ -33,23 +33,23 @@ def fcheck():
     ResultPath = nml['paths']['ResultPath']
     runid = nml['modelname']['runid']
 
-    print(MeshPath)
-    print(ResultPath)
-    print(runid)
-
     fcheck_values_path = os.path.join(args.path, 'fcheck_values.csv')
     if not fcheck_values_path:
         raise FileNotFoundError(f'Can\'t gind {fcheck_values_path}.')
 
     ds = pd.read_csv('./fcheck_values.csv', index_col=0)
 
-    last_year = find_last_year(ResultPath, variable, runid)
     for variable in ds.index:
-        ffile = Dataset(f'{}/{variable}.{runid}.{last_year}.nc')
+        last_year = find_last_year(ResultPath, variable, runid)
+        ffile = Dataset(f'{ResultPath}/{variable}.{runid}.{last_year}.nc')
         current_value = ffile.variables[variable][:].mean()
         master_value = ds.loc[variable].values[0]
-        assert current_value == pytest.approx(master_value)
-
+        print(f"Variable: {variable}, current_value: {current_value}, master_value: {master_value}")
+        try:
+            assert current_value == approx(master_value)
+        except:
+            raise AssertionError(f'For {variable} we expect {master_value}. Got {current_value} instead.')
+    print()
 
 if __name__ == "__main__":
     # args = parser.parse_args()
